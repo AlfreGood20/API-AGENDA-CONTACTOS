655168cfb21c7acdd7535edc35c2dc9337ebd07c
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-10-30 17:48:46 -0600

    Controlador de atenticacion con httpSession

---
+++ src/main/java/com/api/api_contacto/controlador/AccesoController.java
@@ -0,0 +1,46 @@
+package com.api.api_contacto.controlador;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.api.api_contacto.dtos.response.UsuarioResponse;
+import com.api.api_contacto.servicio.AccesoServ;
+import com.api.api_contacto.utils.Login;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class AccesoController {
+
+    private final AccesoServ accesoServ;
+
+    
+
+    public AccesoController(AccesoServ accesoServ) {
+        this.accesoServ = accesoServ;
+    }
+
+
+    @PostMapping("/auth")
+    public ResponseEntity<UsuarioResponse> darAcceso(@Valid @RequestBody Login crendenciales, HttpSession session) {
+        return ResponseEntity.ok().body(accesoServ.autenticar(crendenciales, session));
+    }
+
+
+    @GetMapping("/deauthenticate")
+    public ResponseEntity<Void> cerrarSession(HttpSession session) {
+        accesoServ.desAutenticarse(session);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/perfil")
+    public ResponseEntity<UsuarioResponse> perfilLogiado(HttpSession session) {
+        return ResponseEntity.ok().body(accesoServ.obtenerPerfilLogiado(session));
+    }
+    
+}
---
+++ src/main/java/com/api/api_contacto/servicio/AccesoServ.java
@@ -0,0 +1,50 @@
+package com.api.api_contacto.servicio;
+
+import org.springframework.stereotype.Service;
+import com.api.api_contacto.dtos.response.UsuarioResponse;
+import com.api.api_contacto.exepciones.ExepcionAutenticacionRechazada;
+import com.api.api_contacto.mappers.UsuarioMapper;
+import com.api.api_contacto.modelo.Usuario;
+import com.api.api_contacto.repositorio.UsuarioRepo;
+import com.api.api_contacto.utils.Login;
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class AccesoServ {
+
+    private final UsuarioRepo usuarioRepo;
+    private final UsuarioMapper usuarioMapper;
+
+    public AccesoServ(UsuarioRepo usuarioRepo, UsuarioMapper usuarioMapper) {
+        this.usuarioRepo = usuarioRepo;
+        this.usuarioMapper=usuarioMapper;
+    }
+
+    
+
+    public UsuarioResponse autenticar(Login request, HttpSession session){
+        Usuario usuario = usuarioRepo.findByCorreo(request.getCorreo())
+        .orElseThrow(() -> new ExepcionAutenticacionRechazada("No existe usuario con correo "+request.getCorreo()));
+
+        if(!usuario.getContrasena().equals(request.getContrasena())) throw new ExepcionAutenticacionRechazada("Contraseña incorrecta");
+
+        UsuarioResponse usuarioDto = usuarioMapper.toDto(usuario);
+        session.setAttribute("usuario", usuarioDto);
+        return usuarioDto;
+    }
+
+
+
+    public void desAutenticarse(HttpSession session){
+        session.invalidate();
+    }
+
+
+    
+    public UsuarioResponse obtenerPerfilLogiado(HttpSession session){
+        Object atributo = session.getAttribute("usuario");
+        if(atributo == null)  throw new ExepcionAutenticacionRechazada("No estás autenticado");
+
+        return (UsuarioResponse) atributo;
+    }
+}

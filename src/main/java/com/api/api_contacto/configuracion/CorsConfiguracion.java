caf249e9415bbe522d228cd694d52f6599782ee4
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-11-03 19:53:46 -0600

    Configuracion CORS para que me sirva localmente

---
+++ src/main/java/com/api/api_contacto/configuracion/CorsConfiguracion.java
@@ -0,0 +1,23 @@
+package com.api.api_contacto.configuracion;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguracion {
+
+    @Bean
+    public WebMvcConfigurer configuracion(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}

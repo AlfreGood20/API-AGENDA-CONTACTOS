d1a6a861f6cb0383b21de6d2701c4a5d2a4a1ed7
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-12-30 14:09:18 -0600

    Configuracion para la documentacion

--- src/main/java/com/api/api_contacto/configuracion/OpenApiConfiguracion.java
+++ src/main/java/com/api/api_contacto/configuracion/OpenApiConfiguracion.java
@@ -1,14 +1,11 @@
 package com.api.api_contacto.configuracion;
 
-import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @SecurityScheme(
@@ -18,32 +15,25 @@ import io.swagger.v3.oas.models.servers.Server;
     scheme = "bearer"
 )
 
-public class OpenApiConfiguracion {
+@OpenAPIDefinition(
 
-    @Bean
-    public OpenAPI infoDocumentacion(){
-        Server localServer = new Server();
-        localServer.setUrl("http://localhost:8080/");
-        localServer.description("Servidor URL local");
-
-        Server proServer = new Server();
-        proServer.setUrl("https://");
-        proServer.description("Servidor URL producción");
-
-        Contact contacto = new Contact();
-        contacto.setName("José Alfredo López De La Cruz");
-        contacto.setEmail("josealfredolopezdelacruz2@gmail.com");
-        contacto.setUrl("https://github.com/AlfreGood20");
-
-        Info informacion = new Info();
-        informacion.setTitle("API REST FULL - CONTACTO");
-        informacion.setDescription("Esta api fue construido para usarse en un front end, que se encuentra en mi repositorio de github. Pequeña api para uso educativos y reforzamiento de conocimientos adquiridos durante la practica.");
-        informacion.setContact(contacto);
-        informacion.setVersion("1.0.0");
-
-        return new OpenAPI()
-            .info(informacion)
-            .servers(List.of(localServer,proServer));
+    info = @Info(
+        title = "API REST FULL - CONTACTO",
+        description = "Esta api fue construido para usarse en un front end, que se encuentra en mi repositorio de github. Pequeña api para uso educativos y reforzamiento de conocimientos adquiridos durante la practica.",
+        contact = @io.swagger.v3.oas.annotations.info.Contact(
+            name = "José Alfredo López De La Cruz",
+            email = "josealfredolopezdelacruz2@gmail.com",
+            url = "https://github.com/AlfreGood20"
+        ),
+        version = "2.0.0"
+    ),
+    servers = {
+        @Server(
+            url = "http://localhost:8080/",
+            description = "Servidor localmente"
+        )
     }
+)
 
+public class OpenApiConfiguracion {
 }
